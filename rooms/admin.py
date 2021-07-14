93079cca3eeb897889f6e4eed272dec1f332a7ec
Jang-Jaewon <67135804+Jang-Jaewon@users.noreply.github.com>
2021-07-14 21:07:53 +0900

    More Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,14 +6,120 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
     """Item Admin Dfinition"""
 
-    pass
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
     """Room Admin Dfinition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "country",
+                    "city",
+                    "address",
+                    "price",
+                )
+            },
+        ),
+        (
+            "Times",
+            {
+                "fields": (
+                    "check_in",
+                    "check_out",
+                    "instant_book",
+                )
+            },
+        ),
+        (
+            "Spaces",
+            {
+                "fields": (
+                    "guests",
+                    "beds",
+                    "bedrooms",
+                    "baths",
+                )
+            },
+        ),
+        (
+            "More About the Space",
+            {
+                "fields": (
+                    "amenities",
+                    "facilities",
+                    "house_rule",
+                )
+            },
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)},
+        ),
+    )
+
+    ordering = (
+        "name",
+        "price",
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_out",
+        "check_in",
+        "instant_book",
+        "count_amenities",
+        "count_photos",
+        "total_rating",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rule",
+        "city",
+        "country",
+    )
+
+    search_fields = (
+        "city",
+        "^host__username",
+    )
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rule",
+    )
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    count_amenities.short_description = "amenities"
+
+    def count_photos(self, obj):
+        return obj.photos.count()
+
+    count_photos.short_description = "photos"
 
 
 @admin.register(models.Photo)
