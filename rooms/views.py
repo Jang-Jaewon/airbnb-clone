4b2bdf0e5c385a7c017d91d5ec6579da696f2969
Jang-Jaewon <67135804+Jang-Jaewon@users.noreply.github.com>
2021-07-16 16:24:59 +0900

    Start Django urls and views and templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+
+def all_rooms(request):
+    all_rooms = models.Room.objects.all()
+    context = {"rooms": all_rooms}
+    return render(request, "rooms/home.html", context)
