6fb6ef9b33030b626a7ff8451dcb12c12598c342
Jang-Jaewon <67135804+Jang-Jaewon@users.noreply.github.com>
2021-07-17 19:59:26 +0900

    Create HomeView

--- rooms/views.py
+++ rooms/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    context = {"rooms": all_rooms}
-    return render(request, "rooms/home.html", context)
+class HomeView(ListView):
+    """HomeView Definition"""
+
+    model = models.Room
+    paginate_by = 10  # 👈 한 페이지에 제한할 Object 수
+    paginate_orphans = 5  # 👈 짜투리 처리
+    ordering = "created"  # 👈 정렬 기준
+    page_kwarg = "page"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)  # 기존의 CBV에서 만들어진 것을 가져옵니다:)
+        now = timezone.now()
+        context["now"] = now
+        return context
