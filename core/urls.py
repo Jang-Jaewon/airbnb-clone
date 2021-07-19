eb3022b9796622aa91eec4b711bf05d569e13290
Jang-Jaewon <67135804+Jang-Jaewon@users.noreply.github.com>
2021-07-19 12:26:03 +0900

    Create HomeView and RoomDetail

--- core/urls.py
+++ core/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from rooms import views as room_views
 
 app_name = "core"
-urlpatterns = [path("", room_views.HomeView.as_view(), name="home")]
+urlpatterns = [
+    path("", room_views.HomeView.as_view(), name="home"),
+]
--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,4 @@
-from django.utils import timezone
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -7,14 +6,13 @@ class HomeView(ListView):
     """HomeView Definition"""
 
     model = models.Room
-    paginate_by = 10  # 👈 한 페이지에 제한할 Object 수
-    paginate_orphans = 5  # 👈 짜투리 처리
-    ordering = "created"  # 👈 정렬 기준
-    page_kwarg = "page"
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)  # 기존의 CBV에서 만들어진 것을 가져옵니다:)
-        now = timezone.now()
-        context["now"] = now
-        return context
+
+class RoomDetail(DetailView):
+    """RoomDetail Definition"""
+
+    model = models.Room
